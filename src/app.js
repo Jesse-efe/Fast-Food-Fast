49690087ecc836fdf5bffec673169550501edb94
Jesse Omoefe <jesseefe@gmail.com>
2018-09-07 22:01:54 +0100

    Instal babel and added travis.yml file

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+route(app);
+
+app.listen(3000, () => {
+  console.log('app running on port.');
+});
+
+
+module.exports = app;
---
+++ src/test/appTest.js
@@ -0,0 +1,70 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app.js');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('App.js', () => {
+    describe('/api/v1/orders', () => {
+        it('responds with status of 200', (done) => {
+            chai.request(app)
+            .get('/api/v1/orders')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                done();
+            })
+        })
+    })
+    describe('/api/v1/orders/:id', () => {
+        it('responds with status of 200', (done) => {
+            chai.request(app)
+            .get('/api/v1/orders/1')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                done();
+            })
+        })
+    })
+    describe('/api/v1/orders', () => {
+        it('responds with status of 201', (done) => {
+            chai.request(app)
+            .post('/api/v1/orders')
+            .send({name: 'peter', food: 'yam', price: 4, quantity: 6})
+            .end((err, res) => {
+                expect(res).to.have.status(201);
+                done();
+            })
+        })
+    })
+    describe('resource was created', () => {
+        it('responds with status of 200', (done) => {
+            chai.request(app)
+            .post('/api/v1/orders')
+            .send({name: 'peter', food: 'yam', price: 4, quantity: 6})
+            .then(() => {
+              chai.request(app)
+                .get('/api/v1/orders/last')
+                .end((err, res) => {
+                  expect(res).to.have.status(200);
+                  expect(res.body.customerName).to.equal('peter');
+                  expect(res.body.foodOrdered).to.equal('yam');
+                  expect(res.body.total).to.equal(24);
+                  done();
+                })
+            })
+        })
+    })
+    describe('/api/v1/orders/:id', () => {
+        it('should delete the order', (done) => {
+            chai.request(app)
+            .delete('/api/v1/orders/:id')
+            .end((err, res) => {
+                expect(res).to.have.status(204);
+                done();
+            })
+        })
+    })
+})
