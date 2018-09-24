4e8fad37546139b27db4fef18550fcfb19799420
Jesse Omoefe <jesseefe@gmail.com>
2018-09-24 13:24:02 +0100

    moved files to src folder

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes.js');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 route(app);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('app running on port.');
 });
 
