8bd5b3010b2b83b6025ccfa8c41df62dc96e5b70
Jesse Omoefe <jesseefe@gmail.com>
2018-09-26 23:06:55 +0100

    Add eslint with airbnb style guide [Finishes #160807581]

--- controller/ordersController.js
+++ controller/ordersController.js
@@ -1,57 +1,54 @@
 const orders = require('../orders.js');
 
 class OrdersController {
-    getAllOrders(req, res) {
-        return res.status(200).json(orders);
+  static getAllOrders(req, res) {
+    return res.status(200).json(orders);
+  }
+
+  static getLastOrder(req, res) {
+    const ids = Object.keys(orders);
+    const lastId = parseInt(ids.pop());
+    return res.status(200).json(orders[lastId]);
+  }
+
+  static getAnOrder(req, res) {
+    const { params: { id: orderId } } = req;
+    if (orders[orderId] === undefined) {
+      return res.status(404).send('Invalid order Id');
+    }
+    return res.status(200).json(orders[orderId]);
+  }
+
+  static postAnOrder(req, res) {
+    const ids = Object.keys(orders);
+    const lastId = parseInt(ids.pop());
+    const newId = lastId + 1;
+    orders[newId] = {
+      customerName: req.body.name,
+      foodOrdered: req.body.food,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      total: req.body.price * req.body.quantity,
+      orderStatus: 'unresolved',
     };
+    return res.status(201).send('Your arder was received');
+  }
 
-    getLastOrder(req, res) {
-        let ids = Object.keys(orders);
-        let lastId = parseInt(ids.pop());
-        return res.status(200).json(orders[lastId]);
-    };
+  static updateOrderStatus(req, res) {
+    const { params: { id: orderId } } = req;
+    orders[orderId].orderStatus = 'accepted';
+    return res.status(200).send('Order status has been updated');
+  }
 
-    getAnOrder(req, res) {
-        const { params: { id: orderId } } = req;
-        if(orders[orderId] == undefined){
-            return res.status(404).send('Invalid order Id');
-        }else{
-            return res.status(200).json(orders[orderId]);
-        };
-    };
-
-    postAnOrder(req, res){
-        let ids = Object.keys(orders);
-        let lastId = parseInt(ids.pop());
-        let newId = lastId + 1;
-        orders[newId] = {
-            customerName: req.body.name,
-            foodOrdered: req.body.food,
-            price: req.body.price,
-            quantity: req.body.quantity,
-            total: req.body.price * req.body.quantity,
-            orderStatus: 'unresolved',
-        };
-        return res.status(201).send('Your arder was received');
-        };
-
-        updateOrderStatus(req, res) {
-            const { params: { id: orderId } } = req;
-            orders[orderId].orderStatus = 'accepted';
-            return res.status(200).send('Order status has been updated');
-        };
-
-        deleteAnOrder(req, res) {
-            const { params: { id: orderId } } = req;
-            if(orders[orderId] == undefined){
-                return res.status(404).send('Invalid order Id');
-            }else{
-                delete orders.orderId;
-                return res.status(204).send('Order has been deleted');
-            };
-        };
+  static deleteAnOrder(req, res) {
+    const { params: { id: orderId } } = req;
+    if (orders[orderId] === undefined) {
+      return res.status(404).send('Invalid order Id');
+    }
+    delete orders.orderId;
+    return res.status(204).send('Order has been deleted');
+  }
 }
 
 
-const ordersController = new OrdersController();
-module.exports = ordersController;
+module.exports = OrdersController;
