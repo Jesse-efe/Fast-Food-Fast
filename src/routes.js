67304581cc9f5a5040df3604e2f820b685d4f7ff
Jesse Omoefe <jesseefe@gmail.com>
2018-10-01 01:44:01 +0100

    Make test pass

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import ordersController from './controller/ordersController';
-import user from './controller/users';
 
 
 const Router = (app) => {
@@ -10,9 +9,6 @@ const Router = (app) => {
   app.post('/api/v1/orders', ordersController.postAnOrder);
   app.put('/api/v1/orders/:id', ordersController.updateOrderStatus);
   app.delete('/api/v1/orders/:id', ordersController.deleteAnOrder);
-  app.post('/api/v1/users', user.signUserUp);
-  app.get('/api/v1/users', user.findAllUser);
-  app.get('/api/v1/users/:email', user.findUser);
 };
 
 
