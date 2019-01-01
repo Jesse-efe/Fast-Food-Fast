01f5e52b482527776a9decc1481a17b0ebf07256
Jesse Omoefe <jesseefe@gmail.com>
2019-01-01 10:41:48 +0100

    make travis pass

--- src/db/config.js
+++ src/db/config.js
@@ -3,8 +3,8 @@ import { Pool } from 'pg';
 require('dotenv').config();
 
 const pool = new Pool({
-  //  connectionString: process.env.devConnectionString,
-  connectionString: process.env.testConnectionString,
+  connectionString: process.env.devConnectionString,
+  // connectionString: process.env.testConnectionString,
 });
 
 export default pool;
