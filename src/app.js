9b17701557e8515f35068f2fccd28a39fc000393
Jesse Omoefe <jesseefe@gmail.com>
2019-01-04 02:12:53 +0100

    handle CORS error

--- src/app.js
+++ src/app.js
@@ -10,14 +10,21 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use('/api/v1', route);
 app.use((req, res, next) => {
   const error = new Error('Not found');
   error.status = 404;
   next(error);
 });
-
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({
